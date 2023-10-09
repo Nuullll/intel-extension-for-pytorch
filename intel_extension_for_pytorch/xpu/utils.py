c6ea20bede12f2dbaab8d81d226b4747c0599167
Nuullll <vfirst218@gmail.com>
2023-10-09 20:33:03 +0800

    Revert "Enable quant save/load through prepack fn registration (#3078)"
    This reverts commit 7db2f1c47495e6a26cfd212b191eb8a66524096e.

--- intel_extension_for_pytorch/xpu/utils.py
+++ intel_extension_for_pytorch/xpu/utils.py
@@ -310,6 +310,10 @@ def disable_tile_as_device():
 ################################################################
 
 
+def has_jit_quantization_save():
+    return _C._is_jit_quantization_save_enabled()
+
+
 def has_xetla():
     return _C._is_xetla_enabled()
 
